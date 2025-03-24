c18b36e7ed570f7190278d038002f03fec69b608
Kdub <kalebmay22024@gmail.com>
2025-03-24 15:00:45 -0500

    3.0

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
         <h1 className="errorCode">404</h1>
         <h2 className="errorTitle">Page Not Found</h2>
         <p className="errorMessage">
-          Sorry, we couldn't find the page you're looking for.
+          Sorry, we couldn't find the page you're looking for. Site is in Constant Development
         </p>
         <Link href="/" className="homeButton">
           Return Home
