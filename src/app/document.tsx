13c2d9a4c39afc1f44ea00d256120fc1b344a683
Kdub <kalebmay22024@gmail.com>
2025-03-26 22:18:26 -0500

    3.9.5

--- src/app/document.tsx
+++ src/app/document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4585137285765836"
+     crossOrigin="anonymous"></script>
       </Head>
       <body>
         <div id="preloader">
