d3519926d7e246999d3f24d624e9358a3d18c9d5
Kdub <kalebmay22024@gmail.com>
2025-04-02 16:56:59 -0500

    5.5 SALE 50% OFF

--- src/components/mixingmasteringGrid.tsx
+++ src/components/mixingmasteringGrid.tsx
@@ -16,7 +16,7 @@ export const MixingMasteringGridComponent = ({ setLoading }: MixingMasteringGrid
   const [message, setMessage] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitMessage, setSubmitMessage] = useState('');
-  const [saleActive,] = useState(false); 
+  const [saleActive,] = useState(true); 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
